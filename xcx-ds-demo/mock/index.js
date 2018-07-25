71ed2cf8fc47d72a7d427bb297ffa53c11f076ad
summer326717 <summer326717@foxmail.com>
2018-07-25 14:56:21 +0800

    update

---
+++ xcx-ds-demo/mock/index.js
@@ -0,0 +1,15 @@
+const bannerList = [
+  {
+    bannerUrl: 'https://summer326717.github.io/xcx-ds-demo/images/banner_01.png'
+  },
+  {
+    bannerUrl: 'https://summer326717.github.io/xcx-ds-demo/images/banner_02.png'
+  },
+  {
+    bannerUrl: 'https://summer326717.github.io/xcx-ds-demo/images/banner_03.png'
+  }
+]
+
+export default {
+  bannerList: bannerList
+}
