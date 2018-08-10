aa01f7fafcf24ab730e4158764bb3b2e08dbbc24
summer326717 <summer326717@foxmail.com>
2018-08-10 17:45:34 +0800

    update

---
+++ pocket-xcx/project/pages/agent/homePage/homePage.js
@@ -0,0 +1,92 @@
+// pages/agent/homePage/homePage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menuList: [
+      {
+        menuName: '出纸统计'
+      },
+      {
+        menuName: '设备管理'
+      },
+      {
+        menuName: '缺纸设备'
+      },
+      {
+        menuName: '故障设备'
+      },
+      {
+        menuName: '广告平台'
+      },
+      {
+        menuName: '广告管理'
+      },
+      {
+        menuName: '运营人员'
+      },
+      {
+        menuName: '细分管理'
+      }
+    ],
+    avatarUrl: '', // 头像
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
