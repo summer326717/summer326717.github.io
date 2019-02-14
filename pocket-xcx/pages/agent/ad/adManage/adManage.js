8b4da20965674bb6a5fc18a63f99c66b72e10ee1
summer326717 <summer326717@foxmail.com>
2019-02-14 17:32:21 +0800

    update ad delete

--- pocket-xcx/pages/agent/ad/adManage/adManage.js
+++ pocket-xcx/pages/agent/ad/adManage/adManage.js
@@ -18,18 +18,104 @@ Page({
     pageSize: 10,
     isToBottom: false,
     isShowAddMenu: false,
-    clientX: '',
-    clientY: '',
-    delBtnWidth: 60, // 删除按钮宽度
-    dataList: []
+    dataList: [],
+    startX: 0, //开始坐标
+    startY: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
+  touchstart: function (e) {
+    //开始触摸时 重置所有删除
+    this.data.dataList.forEach(function (v, i) {
+    if (v.isTouchMove)//只操作为true的
+      v.isTouchMove = false;
+    })
+    this.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      dataList: this.data.dataList
+    })
+    },
+    //滑动事件处理
+    touchmove: function (e) {
+    
+    var that = this,
+    
+    index = e.currentTarget.dataset.index,//当前索引
+    
+    startX = that.data.startX,//开始X坐标
+    
+    startY = that.data.startY,//开始Y坐标
+    
+    touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+    
+    touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+    
+    //获取滑动角度
+    
+    angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+    
+    that.data.dataList.forEach(function (v, i) {
+    
+    v.isTouchMove = false
+    
+    //滑动超过30度角 return
+    
+    if (Math.abs(angle) > 30) return;
+    
+    if (i == index) {
+    
+    if (touchMoveX > startX) //右滑
+    
+    v.isTouchMove = false
+    
+    else //左滑
+    
+    v.isTouchMove = true
+    
+    }
+    
+    })
+    //更新数据
+    that.setData({
+      dataList: that.data.dataList
+    })
+    },
+    
+    /**
+    
+    * 计算滑动角度
+    
+    * @param {Object} start 起点坐标
+    
+    * @param {Object} end 终点坐标
+    
+    */
+    
+    angle: function (start, end) {
+    
+    var _X = end.X - start.X,
+    
+    _Y = end.Y - start.Y
+    
+    //返回角度 /Math.atan()返回数字的反正切值
+    
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+    
+    },
+    
+    //删除事件
+    
+    del: function (e) {
+      this.data.dataList.splice(e.currentTarget.dataset.index, 1)
+      this.setData({
+        dataList: this.data.dataList
+      })
+    },
   /**
    * 选择状态
    */
@@ -108,6 +194,9 @@ Page({
         if (this.data.pageNo > 1) {
           result = this.data.dataList
         }
+        res.data.resultList.map((v,i)=>{
+          v.isTouchMove = false
+        })
         let finalResult = util.concattArr(result, res.data.resultList)
         this.setData({
           dataList: finalResult,
@@ -149,28 +238,6 @@ Page({
     /*wx.navigateTo({
       url: '/pages/agent/ad/adHalf/adHalf'
     })*/
-  },
-  deleteTouchStart(e) {
-    this.setData({
-      clientX: e.changedTouches[0].clientX
-    })
-  },
-  deleteTouchmove(e) {
-    let disWidth = this.data.clientX - e.changedTouches[0].clientX
-    let delBtnWidth = this.data.delBtnWidth
-    if (disWidth > 0) {
-      var right = disWidth > delBtnWidth ? 0 : -(60 - disWidth)
-    } else {
-      var right = -disWidth > delBtnWidth ? -60 : disWidth
-    }
-    var index = e.currentTarget.dataset.index
-    var list = this.data.dataList
-    list[index].right = right
-    this.setData({
-      dataList: list
-    })
-  },
-  deleteTouchEnd(e) {
   },
   removeAd(e) {
     wx.showModal({
